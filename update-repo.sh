d4c0e1e4feec3282e389976077c9a9ada9d8e013
Ngoc Quy Le <NgocQuy.Le@encoway.de>
2022-01-05 18:20:11 +0100

    sso'fied script

--- update-repo.sh
+++ update-repo.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# script must be executed from root of the archive, where apt-ftparchive.conf should also be located
+# script must be executed from root of the archive
 # .deb files must be located in a 'pool' subdirectory at the root of the archive
 
 set -eux
 
 DIST=bullseye
 EMAIL=max.mustermann@gmail.com
+ARCH=amd64
+COMPONENT=main
 
-mkdir -p ./dists/${DIST}/main/binary-amd64
+echo "Dir::ArchiveDir ".";
+
+BinDirectory \"pool\" {
+    Packages \"dists/${DIST}/${COMPONENT}/binary-${ARCH}/Packages\";
+}
+
+APT::FTPArchive::Release::Origin \"Quy\";
+APT::FTPArchive::Release::Label \"test apt repo\";
+APT::FTPArchive::Release::Suite \"${DIST}\";
+APT::FTPArchive::Release::Codename \"${DIST}\";
+APT::FTPArchive::Release::Architectures \"${ARCH}\";
+APT::FTPArchive::Release::Components \"${COMPONENT}\";
+" > ./apt-ftparchive.conf
+
+mkdir -p ./dists/${DIST}/${COMPONENT}/binary-${ARCH}
 
 apt-ftparchive generate ./apt-ftparchive.conf
 
